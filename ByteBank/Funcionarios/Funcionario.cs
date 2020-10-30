ca599517898460486f7df41d860803cce1a1276b
DESKTOP-0M0NPU4\Felipe Delmondes <luis.delmondes@outlook.com>
2020-10-30 16:13:33 -0300

    Primeiro Commit

---
+++ ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank;
+
+namespace ByteBank.Funcionarios
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+
+        //0 - Funcionario
+        //1 - Diretor
+        //2 - Designer
+        private int _tipo;
+
+        public Funcionario(int tipo)
+        {
+            _tipo = tipo;
+        }
+
+
+        public double GetBonificacao()
+        {
+       
+            return Salario * 0.10;
+        }
+
+
+
+    }
+}
---
+++ ByteBank/Program.cs
@@ -0,0 +1,36 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario carlos = new Funcionario(1);
+
+            carlos.Nome = "Carlos Almeida";
+            carlos.CPF = "45401384807";
+            carlos.Salario = 5000;
+
+            Console.WriteLine(carlos.GetBonificacao());
+
+
+            Diretor roberta = new Diretor();
+
+            roberta.Nome = "Roberta";
+            roberta.CPF = "45401384807";
+            roberta.Salario = 10000;
+
+            Console.WriteLine(roberta.GetBonificacao());
+
+
+            Console.ReadLine();
+        }
+    }
+}
